cf4ff38b6bce26382c9436618f8fae8eeecd36de
severine-fzl <severine.fuzely@gmail.com>
2022-05-31 16:19:47 +0200

    Affichage dés

--- src/Yams.js
+++ src/Yams.js
@@ -4,14 +4,25 @@ import './Yams.css';
 
 
 
-function Random(){
-  var RandomNumber = Math.floor(Math.random() * 6) + 1 ;
-  return (RandomNumber);
-}
-
-
 function Inscription() {
-  const [tableau, settableau] = useState(null);
+  const [tableau, settableau] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const DiceLength = 5;
+  function shuffleDice(){
+      setIsLoading(false);
+      let dicetab = tableau;
+      if(dicetab.length === 5){
+        dicetab = [];
+      }
+      for (let i = 0; i < DiceLength ; i++) {
+      var RandomNumber = Math.floor(Math.random() * 6) + 1 ;
+      dicetab.push(RandomNumber)
+      console.log(dicetab)
+      } 
+       settableau(dicetab);
+       setIsLoading(true);
+  }
 
   return (
     <div className="App">
@@ -19,7 +30,7 @@ function Inscription() {
       {/* {subscribeconfirm && <p>Inscription Réussie</p>} */}
       <header className="App-header">
         <p>Jeux Yams</p>
-        {tableau != null &&<div className='container'>
+        {isLoading === true &&<div className='container'>
           <div className='dé'>
               <img src={require(`./Assets/${tableau[0]}.png`)}  alt="dé_1" />
           </div>
@@ -36,7 +47,9 @@ function Inscription() {
           <img src={require(`./Assets/${tableau[4]}.png`)} alt="dé_5"  />
           </div>
         </div>}
+        <button onClick={shuffleDice}>Lancer le jeu Yams</button>
       </header>
+  
     </div>
 
   );
